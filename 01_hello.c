7173a350b08d43a0982b3fa33bdfc5ffe51aa1fb
ptran099 <6279104+ptran099@users.noreply.github.com>
2025-10-21 12:47:48 -0400

    finish 01_hello main function

--- 01_hello.c
+++ 01_hello.c
@@ -1,13 +1,18 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_video.h>
 #include <stdio.h>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char *argv[]) {
+	// Window being rendered to
 	SDL_Window* window = NULL;
+	// Surface contained by window
 	SDL_Surface* screenSurface = NULL;
 
+	// Initialize SDL
 	if(SDL_Init(SDL_INIT_VIDEO) < 0) {
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
 	}
@@ -18,24 +23,35 @@ int main(int argc, char *argv[]) {
 				SDL_WINDOWPOS_UNDEFINED,
 				SCREEN_WIDTH,
 				SCREEN_HEIGHT,
-				SDL_WINDOW_SHOWN);
+				SDL_WINDOW_SHOWN
+				);
 		if(!window) {
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
 		}
 		else {
+			// Get window surface
 			screenSurface = SDL_GetWindowSurface(window);
-			SDL_FillRect();
-			SDL_UpdateWindowSurface();
+			// Fill the surface white
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+			// Update the surface
+			SDL_UpdateWindowSurface(window);
+			// Hack to get window to stay
 			SDL_Event e;
-			bool quit = false;
+			int quit = 0;
 			while(!quit) {
-				while() {
-					if() {
+				while(SDL_PollEvent(&e)) {
+					if(e.type == SQL_QUIT) {
+						quit = 0;
 					}
 				}
 			}
 		}
 	}
+
+	// Destroy window
+	SDL_DestroyWindow(window);
+	// Quit SDL subsystems
+	SDL_Quit();
 
 	return 0;
 }
